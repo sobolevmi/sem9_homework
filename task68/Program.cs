88ed9ef954928e549ff7b7bb6e95899e64892f45
Maksim Sobolev <sobmig@gmail.com>
2022-07-24 01:12:30 +0300

    завершено написание кода программы

---
+++ task68/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int AckermannFunction (int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+                return AckermannFunction (m - 1, 1);
+            }
+            else return AckermannFunction (m - 1, AckermannFunction (m, n - 1));
+        }
+
+        Console.Write("Введите неотрицательное целое число m: ");
+        int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите неотрицательное целое число n: ");
+        int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+        if (firstUserNumber >= 0 && secondUserNumber >= 0)
+        {
+            int res = AckermannFunction (firstUserNumber, secondUserNumber);
+            Console.Write($"m = {firstUserNumber}, n = {secondUserNumber} -> A (m, n) = {res}");
+        }
+        else Console.WriteLine("Числа m и n не могут быть отрицательными. Повторите ввод");
+    }
+}
