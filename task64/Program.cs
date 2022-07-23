20da3a64b11c1a03c3a1fc94d8a26572d28f68ab
Maksim Sobolev <sobmig@gmail.com>
2022-07-23 22:21:45 +0300

    завершено написание кода программы

---
+++ task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая выведет все значения натуральных чисел в промежутке от M до N
+string NumbersFromTo (int startUserNumber, int endUserNumber)
+{
+    if (startUserNumber == endUserNumber)
+    return startUserNumber.ToString ();
+    else
+    {
+        string userNumbers = String.Empty;
+        userNumbers = startUserNumber.ToString () + " " + NumbersFromTo (startUserNumber + 1, endUserNumber);
+        return userNumbers;
+    }
+
+}
+
+Console.WriteLine ("Введите число M: ");
+int m = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число N: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+if (n > m)
+{
+    Console.WriteLine ($"M = {m}; N = {n} -> {NumbersFromTo (m, n)}");
+}
+else Console.WriteLine ("Число N должно быть больше числа M, попробуйте заново");
