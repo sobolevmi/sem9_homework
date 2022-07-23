d2a3533db0ab1bc2952e3340561b73ebeed39133
Maksim Sobolev <sobmig@gmail.com>
2022-07-23 22:33:39 +0300

    завершено написание кода программы

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая выводит сумму натуральных чисел в промежутке от M до N
+int SumOfNumbers (int startUserNumber, int endUserNumber)
+{
+    if (startUserNumber == endUserNumber)
+    return startUserNumber;
+    else
+    {
+        int result = 0;
+        result = startUserNumber + SumOfNumbers (startUserNumber + 1, endUserNumber);
+        return result;
+    }
+
+}
+
+Console.WriteLine ("Введите число M: ");
+int m = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число N: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+if (n > m)
+{
+    Console.WriteLine ($"M = {m}; N = {n} -> {SumOfNumbers (m, n)}");
+}
+else Console.WriteLine ("Число N должно быть больше числа M. Попробуйте снова");
